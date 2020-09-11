6ef647aa7a0963d404eefe60f1c44a22f7971041
Jerzy9 <zbylutkonrad@gmail.com>
2020-09-11 23:46:21 +0200

    list works

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,42 +1,31 @@
-import React from "react"
+import React, { Component } from 'react'
 
 function MyApp() {
     return(
-     <div>  
-        <h1>
-            Konrad
-        </h1>
-        <p>blabla bla balbalbalblablalblablalbla</p>
-        <ul>
-            <li> 1 </li>
-            <li> 2 </li>
-            <li> 3 </li>
-        </ul> 
-        <br /><br /><br />
+     <div>   
         <Main />
-        <br /><br /><br />
-        <Footer />
     </div>
     )
 }
 
-function Main() {
-    return (
-    <main>
-        <p>MAIN SECTION</p>
-    </main>
-    )
-}
-    
-
-function Footer() {
-    return(
+class Main extends Component {
+    render() {
+      const names = ['1', '2', '3', '5'];
+   
+      return (
         <div>
-            <footer>
-                <p>footer</p>
-            </footer>
+            {names.map((value, index) => {
+                return <h1 key={index}>{value}</h1>;
+            })}
+          
         </div>
-    )
-}
-
+      );
+    }
+  }
+   
+//   class Greeting extends Component {
+//     render() {
+//       return 
+//     }
+//   }
 export default MyApp
