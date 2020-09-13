c93f30cc5dbdc3c83e7fbf786937843f11e76609
Jerzy9 <zbylutkonrad@gmail.com>
2020-09-13 17:24:52 +0200

    good day

--- src/MyApp.js
+++ src/MyApp.js
@@ -4,7 +4,8 @@ import './style.css'
 function MyApp() {
     return(
      <div className="container">
-        <Navbar />   
+        <Navbar />  
+        <Timer /> 
         <Main />
     </div>
     )
@@ -38,6 +39,24 @@ class Main extends Component {
         </p>
       </div>
     )
+  }
+  
+  class Timer extends Component {
+    render() {
+      const date = new Date()
+      const hours = date.getHours()
+      let timeOfDay
+
+      if(hours < 12 && hours >= 4) timeOfDay = 'morning'
+      else if(hours >= 12 && hours < 18) timeOfDay = 'afternoon'
+      else timeOfDay = 'night'
+
+      return (
+        <div className="timer">
+          <div>Good {timeOfDay}</div>
+        </div>
+      )
+    }
   }
    
 
