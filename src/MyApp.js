457555d9c5ef35c768070cd06a583ca094cf124c
Jerzy9 <zbylutkonrad@gmail.com>
2020-09-15 20:34:25 +0200

    jokes

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,63 +1,50 @@
 import React, { Component } from 'react'
 import './style.css'
 
-function MyApp() {
-    return(
-     <div className="container">
-        <Navbar />  
-        <Timer /> 
-        <Main />
-    </div>
-    )
-}
-
-class Main extends Component {
-    render() {
-      const names = ['1', '2', '3', '5'];
-   
-      return (
-        <div className="tasks">
-            {names.map((value, index) => {
-                // return <h1 key={index}>{value}</h1>;
-                return (
-                  <div key = {index} className="task">
-                    <input className="checkbox" type="checkbox"/>
-                    <label className="paragraph"> Task numer {value} </label>
-                  </div>
-                )
-            })}
-        </div>
-      );
+const jokes = [
+    {
+      id: 1,
+      punchLine:"punch line1",
+      question:"question1",
+    },
+    {
+      id: 2,
+      punchLine:"punch line2",
+      question:"question2",
+    },
+    {
+      id: 3,
+      punchLine:"punch line3",
+      question:"question3",
     }
-  }
+  ]
 
-  function Navbar() {
-    return (
-      <div className="navbar">
-        <p>
-          TODO apliacation
-        </p>
+function MyApp() {
+  
+  jokes.array.forEach(element => {
+    return(
+      <div className="container">
+        <Joke punchLine={jokes.line[0].punchLine} question={jokes.line[0].question}/>
       </div>
     )
-  }
-  
-  class Timer extends Component {
-    render() {
-      const date = new Date()
-      const hours = date.getHours()
-      let timeOfDay
+  });
+    
+}
 
-      if(hours < 12 && hours >= 4) timeOfDay = 'morning'
-      else if(hours >= 12 && hours < 18) timeOfDay = 'afternoon'
-      else timeOfDay = 'night'
+function Joke(props) {
+  return (
+    <div className="joke">
+      <ul>
+        <li>
+          {props.question}
+        </li>
+        <li>
+          {props.punchLine}
+        </li>
+      </ul>
+    </div>
+  )
+}
 
-      return (
-        <div className="timer">
-          <div>Good {timeOfDay}</div>
-        </div>
-      )
-    }
-  }
-   
 
 export default MyApp
