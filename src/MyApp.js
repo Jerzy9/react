050e1f2da64e95ede941a0424fc4df79cf640ce6
Jerzy9 <zbylutkonrad@gmail.com>
2020-09-15 21:01:56 +0200

    products impoer from other file

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,50 +1,17 @@
 import React, { Component } from 'react'
 import './style.css'
+import products from './productsData'
 
-const jokes = [
-    {
-      id: 1,
-      punchLine:"punch line1",
-      question:"question1",
-    },
-    {
-      id: 2,
-      punchLine:"punch line2",
-      question:"question2",
-    },
-    {
-      id: 3,
-      punchLine:"punch line3",
-      question:"question3",
-    }
-  ]
+
 
 function MyApp() {
+
+  let names = [];
+
+  return (products.forEach((product, key) => {
+    return (product.name)
+  }))
   
-  jokes.array.forEach(element => {
-    return(
-      <div className="container">
-        <Joke punchLine={jokes.line[0].punchLine} question={jokes.line[0].question}/>
-      </div>
-    )
-  });
-    
 }
-
-function Joke(props) {
-  return (
-    <div className="joke">
-      <ul>
-        <li>
-          {props.question}
-        </li>
-        <li>
-          {props.punchLine}
-        </li>
-      </ul>
-    </div>
-  )
-}
-
 
 export default MyApp
