24f91b33a144131ff6c0e0b5f3a6c1cc6c6f48c9
Jerzy9 <zbylutkonrad@gmail.com>
2020-09-16 22:06:11 +0200

    array.map

--- src/MyApp.js
+++ src/MyApp.js
@@ -6,12 +6,24 @@ import products from './productsData'
 
 function MyApp() {
 
-  let names = [];
+   let names = products.map((product, key) => <Name key={key} id={product.id} name={product.name} /> )
 
-  return (products.forEach((product, key) => {
-    return (product.name)
-  }))
+  return (
+  <div>
+      {names}
+  </div>
+  )
   
+}
+
+function Name(props) {
+
+  return (
+    <div> 
+      <p>{props.id}   {props.key}    .</p>
+      <h3>{props.name}</h3>
+    </div>
+  )
 }
 
 export default MyApp
