284bf8627df8c4cf38a4360448d6322ca2f6ce30
Jerzy9 <zbylutkonrad@gmail.com>
2020-09-17 19:19:29 +0200

    class

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react'
 import './style.css'
 import products from './productsData'
+import { render } from 'react-dom'
 
 
 
-function MyApp() {
+class MyApp extends React.Component{
+  render() {
+    let names = products.map((product) => <Name product={product} /> )
 
-   let names = products.map((product) => <Name product={product} /> )
-
-  return (
-  <div>
-      {names}
-  </div>
-  )
-  
+    return (
+      <div>
+          {names}
+      </div>
+    )
+  }
 }
 
-function Name(props) {
+class Name extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      answer: "Yes",
+    }
+
+  }
+
+  render() {
+
     return (
       <div style={{display:"flex"}}>
-        <input type="checkbox" checked={props.product.id[0]>5 ? true : false}/> 
-        <p>{props.product.id}</p>
-        <h3 style={{marginBottom: 50}}> {props.product.name} </h3>
+        <input type="checkbox" checked={this.props.product.id[0]>5 ? true : false}/> 
+        <p>{this.props.product.id}</p>
+        <h3 style={{marginBottom: 50}}> {this.props.product.name} </h3>
+        <h2>{this.state.answer == "True" ? this.state.answer = "True" : this.state.answer = "Trufe"}</h2>
         
       </div>
     )
+  }
 }
 
 export default MyApp
