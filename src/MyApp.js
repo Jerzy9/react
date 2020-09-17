1558b5bddf7794db7f1639d61eeaa629473e51f7
Jerzy9 <zbylutkonrad@gmail.com>
2020-09-17 20:02:44 +0200

    state

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,43 +1,35 @@
 import React, { Component } from 'react'
 import './style.css'
 import products from './productsData'
-import { render } from 'react-dom'
+// import { render } from 'react-dom'
 
 
 
-class MyApp extends React.Component{
-  render() {
-    let names = products.map((product) => <Name product={product} /> )
-
-    return (
-      <div>
-          {names}
-      </div>
-    )
-  }
-}
-
-class Name extends React.Component {
+class MyApp extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      answer: "Yes",
+      count:0,
     }
-
+    this.handleClick = this.handleClick.bind(this)
+  }
+  
+  handleClick(i) {
+    i++;
+    this.setState({ count: i})
   }
 
   render() {
-
-    return (
-      <div style={{display:"flex"}}>
-        <input type="checkbox" checked={this.props.product.id[0]>5 ? true : false}/> 
-        <p>{this.props.product.id}</p>
-        <h3 style={{marginBottom: 50}}> {this.props.product.name} </h3>
-        <h2>{this.state.answer == "True" ? this.state.answer = "True" : this.state.answer = "Trufe"}</h2>
-        
-      </div>
-    )
+    let i = 0;
+      return (
+        <div >
+          <h1>{this.state.count}</h1>
+           <button onClick={this.handleClick(i)}>Change</button>
+        </div>
+      );
+    
   }
 }
+
 
 export default MyApp
